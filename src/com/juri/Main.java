9bd8dab4b132a3783d1d4b1b79e9b1641f4f67f0
Juri <juri.bojarski@gmail.com>
2020-01-22 21:36:20 +0200

    22.01 Pervaja zada4a

--- src/com/juri/Main.java
+++ src/com/juri/Main.java
@@ -13,10 +13,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        for (int count = 100; count < 999; count++){
+
+            if (count % 11 == 0) {
+                System.out.println(count + " delitsa na 11");
+            } else {
+                System.out.println(count);
+            }
+
+        }
+
+    }
+
 
         // todo: 22.01
         // написать программу поиска всех трёхзначных чисел, которые деляться на 11
         // вывести их все на консоль и посчитать их кол-во
-
-    }
 }
