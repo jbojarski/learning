5b7a007ee295e568193da8246d0545296c297290
andrei prokofjev <andrei.prokofjev@trueapp.co>
2020-01-22 17:48:21 +0200

    remove module

---
+++ src/com/juri/Main.java
@@ -0,0 +1,22 @@
+
+
+package com.juri;
+
+/**
+ * 1) примитивные типы int, byte, float, long, double
+ * 2) цикл for, while do
+ * 3)
+ */
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        // todo: 22.01
+        // написать программу поиска всех трёхзначных чисел, которые деляться на 11
+        // вывести их все на консоль и посчитать их кол-во
+
+    }
+}
