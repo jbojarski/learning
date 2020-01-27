3387f690c38c9c38c5e04a73f6bc8cd01ef4dad3
Juri <juri.bojarski@gmail.com>
2020-01-27 20:42:42 +0200

    Logical operators

--- src/com/juri/task2.java
+++ src/com/juri/task2.java
@@ -19,5 +19,80 @@ public class task2 {
 
         // todo: calculate logical values
 
+        int a = 10;
+        int b = 20;
+        int c = 20;
+
+        // using true   AND   true
+        if ((a < b) && (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+        // using false   AND   true
+        if ((a > b) && (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using false   AND   false
+        if ((a > b) && (b != c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using true   OR   true
+        if ((a < b) || (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using false   OR   true
+        if ((a > b) || (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using false   OR   false
+        if ((a > b) || (b != c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using true   XOR   true
+        if ((a < b) ^ (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using true   XOR   false
+        if ((a < b) ^ (b != c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using false   XOR   false
+        if ((a > b) ^ (b != c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
+        // using false   XOR   true
+        if ((a > b) ^ (b == c)) {
+            System.out.println("True");
+        } else
+            System.out.println("False");
+
+
     }
+
+
 }
