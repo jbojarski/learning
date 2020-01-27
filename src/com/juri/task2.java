1c29d51838d8ff79c622fd6eb0757e5ec4e76e84
andrei prokofjev <andrei.prokofjev@trueapp.co>
2020-01-27 11:03:35 +0200

    add task

--- src/com/juri/task2.java
+++ src/com/juri/task2.java
@@ -2,7 +2,22 @@ package com.juri;
 
 public class task2 {
 
+    // !------!-----!------!---
+    //  true   AND   true   =  ?
+    //  false  AND   true   =  ?
+    //  false  AND   false  =  ?
+    //  true   OR    true   =  ?
+    //  false  OR   true   =  ?
+    //  false  OR   false  =  ?
+    //  true   XOR   true   = ?
+    //  true   XOR   false   = ?
+    //  false   XOR   false   = ?
+    //  false   XOR   true   = ?
+    // !------!-----!------!---
+
     public static void main(String[] args) {
+
+        // todo: calculate logical values
 
     }
 }
