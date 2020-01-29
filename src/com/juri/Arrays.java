877d0e538f7b1de4a5f4c4a47e75b0aad9133a8b
Juri <juri.bojarski@gmail.com>
2020-01-29 09:52:04 +0200

    average, max value

--- src/com/juri/Arrays.java
+++ src/com/juri/Arrays.java
@@ -8,12 +8,44 @@ public class Arrays {
 
         Random rnd = new Random(); // генератор случайных чисел
 
-        int[] array = new int[50];      // массив из 50-ти элементов
+        int[] array = new int[10];     // массив из 50-ти элементов
 
+        printArray(array);
+
+        //1) распечатать через запятую все элементы массива (в одну строчку)
         for (int i = 0; i < array.length; i++) {
             int nextRandomInt = rnd.nextInt(100); // получение случайного числа в пределах от 0 до 100
-            array[i] = nextRandomInt;        // инициализация элемента массива
+            array[i] = nextRandomInt; // init
         }
+        System.out.println();
+        printArray(array);
+
+        System.out.println();
+        System.out.println();
+
+
+        // 2) создать новый массив и заполнить его значениями из первого массива в обратном порядке
+
+        int[] reverse = new int[array.length];
+        for (int i = 0, j = reverse.length - 1; i < reverse.length; i++, j--) {
+            reverse[j] = array[i];
+        }
+
+        printArray(reverse);
+
+        int sum = 0;
+
+
+        for (int i = 0; i < reverse.length; i++) {
+            sum += reverse[i];
+        }
+
+        System.out.println("\n>>> sum: " + sum);
+        System.out.println(">>> average: " + (sum / reverse.length));
+
+        int maxVal = foundMaxValue(reverse);
+        System.out.println(">>> max val: " + maxVal);
+
 
         // 1) распечатать через запятую все элементы массива (в одну строчку)
         // 2) создать новый массив и заполнить его значениями из первого массива в обратном порядке
@@ -22,5 +54,22 @@ public class Arrays {
 
     }
 
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "; ");
+        }
+    }
 
+    private static int foundMaxValue(int[] array) {
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+
+        return max;
+    }
 }
