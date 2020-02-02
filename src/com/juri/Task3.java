1d1936ed0de1d13ba273654ab68e0b3c406fdc09
Juri <juri.bojarski@gmail.com>
2020-02-02 20:23:17 +0200

    Task 3

--- src/com/juri/Task3.java
+++ src/com/juri/Task3.java
@@ -2,4 +2,16 @@ package com.juri;
 
 public class Task3 {
     // вывести все числа от 1000 до -1000 с шагом 25
+
+    public static void main(String[] args) {
+
+        for (int i = 1000; i >= -1000; i--) {
+            if (i % 25 == 0) {
+                System.out.println(i + ", ");
+
+            }
+        }
+    }
+
+
 }
